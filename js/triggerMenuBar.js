b4a4439a9c3d04d4896e810326d8bc9914a88eee
Alexander Dean <alexjacobdean1@gmail.com>
2023-02-12 17:34:46 -0600

    Fix menu bar issues

--- js/triggerMenuBar.js
+++ js/triggerMenuBar.js
@@ -1,9 +1,4 @@
 function triggerMenu() {
     let menu_items = document.getElementById("myMenu");
-
-    if(menu_items.className === "menu") {
-        menu_items.className += " responsive";
-    } else {
-        menu_items.className = "menu";
-    }
+    menu_items.className = (menu_items.className === "menu") ? "menu responsive" : "menu";
 }
