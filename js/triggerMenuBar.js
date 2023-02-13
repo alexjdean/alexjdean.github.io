689e0700bbb5442b98f140af3c5ad1c25cf5dda2
Alexander Dean <alexjacobdean1@gmail.com>
2023-02-12 19:25:49 -0600

    Menu bar revamp

--- js/triggerMenuBar.js
+++ js/triggerMenuBar.js
@@ -1,4 +1,9 @@
 function triggerMenu() {
     let menu_items = document.getElementById("myMenu");
-    menu_items.className = (menu_items.className === "menu") ? "menu responsive" : "menu";
+
+    if(menu_items.className === "menu") {
+        menu_items.className += " responsive";
+    } else {
+        menu_items.className = "menu";
+    }
 }
