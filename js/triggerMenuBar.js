1256943c1a1c0d7db7204897d8ac17a4d7ab16aa
Alex Dean <alexjacobdean1@gmail.com>
2024-07-06 14:30:43 -0500

    Remove styling and old resume

--- js/triggerMenuBar.js
+++
@@ -1,9 +0,0 @@
-function triggerMenu() {
-    let menu_items = document.getElementById("myMenu");
-
-    if(menu_items.className === "menu") {
-        menu_items.className += " responsive";
-    } else {
-        menu_items.className = "menu";
-    }
-}
